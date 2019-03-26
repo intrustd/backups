537a19bda5de7431f432440e29c70422f08ecd40
Travis Athougies <travis@athougies.net>
2019-03-25 22:59:32 -0700

    Disable the socat service to see if it works

--- jobsets.nix
+++ jobsets.nix
@@ -1,30 +1,30 @@
-let pkgs = import <nixpkgs> {};
-in {
-  jobsets =
-    let spec = {
-          app = {
-            description = "Intrustd Backup App";
-            enabled = 1;
-            hidden = false;
-            nixexprinput = "src";
-            nixexprpath = "build-hydra-test.nix";
-            checkinterval = 300;
-            schedulingshares = 50;
-            enableemail = true;
-            emailoverride = "";
-            keepnr = 3;
-            inputs = {
-              src = { type = "git"; value = "git://github.com/intrustd/backups.git"; emailresponsible = true; };
-              nixpkgs = { type = "git"; value = "git://github.com/intrustd/nixpkgs.git intrustd"; emailresponsible = true; };
-              intrustd = { type = "git"; value = "git://github.com/intrustd/daemon.git"; emailresponsible = true; };
-              system = { type = "git"; value = "git://github.com/intrustd/appliance.git"; emailresponsible = true; };
-            };
-          };
-        };
-    in pkgs.writeText "spec.json" (builtins.toJSON spec);
+#let pkgs = import <nixpkgs> {};
+#in {
+#  jobsets =
+#    let spec = {
+#          app = {
+#            description = "Intrustd Backup App";
+#            enabled = 1;
+#            hidden = false;
+#            nixexprinput = "src";
+#            nixexprpath = "build-hydratnix/build-hydra.nix";
+#            checkinterval = 300;
+#            schedulingshares = 50;
+#            enableemail = true;
+#            emailoverride = "";
+#            keepnr = 3;
+#            inputs = {
+#              src = { type = "git"; value = "git://github.com/intrustd/backups.git"; emailresponsible = true; };
+#              nixpkgs = { type = "git"; value = "git://github.com/intrustd/nixpkgs.git intrustd"; emailresponsible = true; };
+#              intrustd = { type = "git"; value = "git://github.com/intrustd/daemon.git"; emailresponsible = true; };
+#              system = { type = "git"; value = "git://github.com/intrustd/appliance.git"; emailresponsible = true; };
+#            };
+#          };
+#        };
+#    in pkgs.writeText "spec.json" (builtins.toJSON spec);
+#}
+#
+import <intrustd/nix/hydra-app-jobsets.nix> {
+  description = "Intrustd Backup App";
+  src = { type = "git"; value = "git://github.com/intrustd/backups.git"; emailresponsible = true; };
 }
-
-# import <intrustd/nix/hydra-app-jobsets.nix> {
-#   description = "Intrustd Backup App";
-#   src = { type = "git"; value = "git://github.com/intrustd/backups.git"; emailresponsible = true; };
-# }
